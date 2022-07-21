83148c3480aa8dfc7b7e9d72ff13507b3ee5e1bd
Łukasz Marczak <mrcl@viessmann.com>
2022-07-21 08:42:50 +0200

    possibility to pass composable to button content

--- app/src/main/java/pl/lukaszmarczak/neopop/compose/MainActivity.kt
+++ app/src/main/java/pl/lukaszmarczak/neopop/compose/MainActivity.kt
@@ -1,66 +1,77 @@
+@file:OptIn(ExperimentalUnitApi::class)
+
 package pl.lukaszmarczak.neopop.compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import pl.lukaszmarczak.neopop.compose.ui.theme.NeoPOPComposeTheme
 import pl.lukaszmarczak.neopop.compose.ui.theme.NeoPopButton
 
 class MainActivity : ComponentActivity() {
-    class VM : ViewModel() {
-        val state = MutableLiveData(false)
-    }
-
-    val viewModel = VM()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NeoPOPComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.LightGray),
-                    color = MaterialTheme.colors.background
+                        .fillMaxSize(),
+                    color = Color.Black
                 ) {
-                    Column(Modifier.fillMaxWidth()) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Greeting("NEOPOP")
+                        Spacer(Modifier.size(100.dp))
+                        NeoPopButton(
+                            buttonColor = Color.Yellow,
+                            edgesColors = Color.Gray to Color.LightGray,
+                            modifier = Modifier.size(150.dp, 50.dp),
+                            buttonContent = {
+                                Text(text = "Accept", textAlign = TextAlign.Center)
+                            }
+                        )
+                        NeoPopButton(
+                            buttonColor = Color.Magenta,
+                            edgesColors = Color.LightGray to Color.Gray,
+                            modifier = Modifier.size(150.dp, 50.dp),
+                            buttonContent = {
+                                Text(
+                                    color = Color.White,
+                                    text = "Reject",
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        )
+                        NeoPopButton(
+                            buttonColor = Color.Cyan,
+                            edgesColors = Color.Gray to Color.LightGray,
+                            modifier = Modifier.size(150.dp, 50.dp),
+                            buttonContent = {
+                                Text(text = "Cancel", textAlign = TextAlign.Center)
+                            }
+                        )
 
-                        Greeting("Hello world!")
-                        Spacer(Modifier.size(148.dp))
-
-                        Row(modifier = Modifier.padding(24.dp)) {
-                            NeoPopButton(
-                                buttonColor = Color.Yellow,
-                                buttonText = "Button",
-                                edgesColors = Color.DarkGray to Color.Gray,
-                                modifier = Modifier.size(200.dp, 48.dp),
-                            )
-                            NeoPopButton(
-                                buttonColor = Color.Cyan,
-                                buttonText = "Button",
-                                edgesColors = Color.Gray to Color.Green,
-                                modifier = Modifier.size(200.dp, 48.dp),
-                            )
-                        }
                     }
                 }
             }
@@ -69,14 +80,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NeoPOPComposeTheme {
-        Greeting("Android")
-    }
+fun ColumnScope.Greeting(name: String) {
+    Spacer(modifier = Modifier.height(100.dp))
+    Text(
+        text = "Hello $name!",
+        color = Color.White,
+        fontSize = TextUnit(24f, TextUnitType.Sp)
+    )
 }
--- app/src/main/java/pl/lukaszmarczak/neopop/compose/ui/theme/NeoPopButton.kt
+++ app/src/main/java/pl/lukaszmarczak/neopop/compose/ui/theme/NeoPopButton.kt
@@ -1,7 +1,5 @@
 package pl.lukaszmarczak.neopop.compose.ui.theme
 
-import android.graphics.Paint
-import android.graphics.Typeface
 import androidx.annotation.FloatRange
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -9,6 +7,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,17 +17,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.DrawModifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
-import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NeoPopButton(
     buttonColor: Color,
-    buttonText: String,
     edgesColors: Pair<Color, Color>,
     modifier: Modifier = Modifier,
+    buttonContent: @Composable BoxScope.() -> Unit
 ) {
+    val depthSize = Size(35f, 30f)
     val state = remember { mutableStateOf(false) }
     val pressingProgress: Float by animateFloatAsState(
         targetValue = if (state.value) 1f else 0f,
@@ -36,8 +40,8 @@ fun NeoPopButton(
     Box(
         modifier = modifier
             .background(Color.Transparent)
-            .neoPopEdges(edgesColors, pressingProgress)
-            .neoPopArea(buttonColor, buttonText, pressingProgress)
+            .neoPopEdges(edgesColors, depthSize, pressingProgress)
+            .neoPopArea(buttonColor, depthSize, pressingProgress)
             .clickable(
                 interactionSource = interactionSource,
                 indication = null,
@@ -47,30 +51,35 @@ fun NeoPopButton(
             ),
         contentAlignment = Alignment.Center
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(
+                    x = ((depthSize.width / 2) * pressingProgress).dp,
+                    y = ((depthSize.height / 2) * pressingProgress).dp
+                ),
+            contentAlignment = Alignment.Center,
+            content = buttonContent
+        )
     }
 }
 
 fun Modifier.neoPopArea(
     buttonColor: Color,
-    buttonText: String,
+    depthSize: Size,
     @FloatRange(from = 0.0, to = 1.0) depth: Float
 ) = then(
     object : DrawModifier {
-        val paint = Paint().apply {
-            textSize = 40f
-            typeface = Typeface.DEFAULT_BOLD
-            color = android.graphics.Color.BLACK
-            textAlign = Paint.Align.CENTER
-        }
-
+        private val path = Path()
         override fun ContentDrawScope.draw() {
             val (width, height) = size
-            val xOffset = 30f * depth
-            val yOffset = 30f * depth
+            val xOffset = depthSize.width * depth
+            val yOffset = depthSize.height * depth
 
             drawPath(
                 color = buttonColor,
-                path = androidx.compose.ui.graphics.Path().apply {
+                path = path.apply {
+                    reset()
                     moveTo(xOffset, yOffset)
                     lineTo(width + xOffset, yOffset)
                     lineTo(width + xOffset, height + yOffset)
@@ -78,45 +87,45 @@ fun Modifier.neoPopArea(
                     close()
                 }
             )
-            drawContext.canvas.nativeCanvas.apply {
-                drawText(
-                    buttonText,
-                    (size.width + xOffset) / 2,
-                    ((size.height + yOffset) / 2) + 20,
-                    paint
-                )
-            }
             drawContent()
         }
     }
 )
 
-fun Modifier.neoPopEdges(edgesColors: Pair<Color, Color>, pressed: Float) = then(
+fun Modifier.neoPopEdges(
+    edgesColors: Pair<Color, Color>,
+    depthSize: Size,
+    pressed: Float
+) = then(
     object : DrawModifier {
+        private val horizontalPath = Path()
+        private val verticalPath = Path()
 
         override fun ContentDrawScope.draw() {
             val (width, height) = size
-            val absoluteOffsetX = 30f
-            val absoluteOffsetY = 30f
-            val xOffset = 30f * (pressed)
-            val yOffset = 30f * (pressed)
+            val absoluteOffsetX = depthSize.width
+            val absoluteOffsetY = depthSize.height
+            val xOffset = absoluteOffsetX * pressed
+            val yOffset = absoluteOffsetY * pressed
             drawPath(
                 color = edgesColors.first,
-                path = androidx.compose.ui.graphics.Path().apply {
+                path = verticalPath.apply {
+                    reset()
                     moveTo(width + absoluteOffsetX, height + absoluteOffsetY)
                     lineTo(width, height)
                     lineTo(width, yOffset)
-                    lineTo(width + absoluteOffsetX, absoluteOffsetY + yOffset)
+                    lineTo(width + absoluteOffsetX, absoluteOffsetY)
                     close()
                 }
             )
             drawPath(
                 color = edgesColors.second,
-                path = androidx.compose.ui.graphics.Path().apply {
+                path = horizontalPath.apply {
+                    reset()
                     moveTo(width + absoluteOffsetX, height + absoluteOffsetY)
                     lineTo(width, height)
                     lineTo(xOffset, height)
-                    lineTo(xOffset + absoluteOffsetX, height + absoluteOffsetY)
+                    lineTo(absoluteOffsetX, height + absoluteOffsetY)
                     close()
                 }
             )
